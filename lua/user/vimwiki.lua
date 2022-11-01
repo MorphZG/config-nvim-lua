2597b3ffd1247e79ee888b4b466c4079f6a67867
MorphZG <zoran1607@gmail.com>
2022-11-01 18:49:17 +0100

    minor update

--- lua/user/vimwiki.lua
+++ lua/user/vimwiki.lua
@@ -1,12 +1,16 @@
 --change default directory, type :help vimwiki_list
 --you can have different wikis with different options in list
 --for better clarity consider defining varibles for every wiki: wiki1, wiki2...
-let g:vimwiki_list = [{'path': '~/coding_documents/vimwiki/', 
-                      \'path_html': '~/Documents/vimwiki_html/',
-                     \'syntax': 'markdown', 'ext': '.md'}]
+
+-- to run vimscript from .lua file use this syntax:
+--vim.cmd([[vimscript here]])
+
+vim.cmd([[
+let g:vimwiki_list = [{'path': '~/coding_documents/vimwiki/',
+                      \ 'syntax': 'markdown', 'ext': '.md'}]
+]])
 
 --vimwiki can use different styles of markup syntax
 --remove upper comment to change default vimwiki syntax to markdown
 --.md can not convert to html at moment.
-
 
